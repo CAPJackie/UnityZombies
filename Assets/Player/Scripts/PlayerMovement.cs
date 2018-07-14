cea301b0cafa40145b496e26d0bfe73c1a474504
Juan David Ramirez Mendoza <juan.ramirez-me@mail.escuelaing.edu.co>
2018-07-14 08:21:23 -0500

    Game commited

---
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	public float speed = 50.0F;
+	
+	public float jumpForce = 100.0F;
+	
+	
+	private Rigidbody rb;
+	
+	private bool isGrounded;
+	
+
+	void Start () {
+		isGrounded = true;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
+		float straffe = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
+		
+		if(Input.GetButtonDown("Jump") && isGrounded){
+			rb.AddForce((new Vector3(0.0F, 2.0F, 0.0F)) * jumpForce , ForceMode.Impulse);
+		}
+		
+		transform.Translate(straffe,0,translation);
+		
+	}
+	void OnCollisionExit(){
+		isGrounded=false;
+	}
+	void OnCollisionStay(){
+		isGrounded = true;
+	}
+}
---
+++ Assets/Zombie/Scripts/MovementScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+	
+	public float speed = 0.03F;
+	
+	public Transform playerPosition;
+	
+	void FixedUpdate () {
+		transform.position = Vector3.Lerp(transform.position, playerPosition.position, Time.deltaTime * speed);
+	}
+}
